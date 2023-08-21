b94e7371207682fced6fdaf49616adf051b2db68
moriartym <121612416+moriartym@users.noreply.github.com>
2023-08-21 19:44:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,11 @@ const successContainer = document.getElementById("success-container");
 const dismissButton= document.getElementById("dismiss-button");
 const rightContainer = document.getElementById("right-container");
 const lelfContainer = document.getElementById("lelf-container");
+const strongEmail = document.getElementById("strong");
+const emailError = document.getElementById("email-error");
+const emailInput = document.getElementById("emailtext");
 
 
-subscribeButton.addEventListener("click",()=>{
-    successContainer.classList.toggle("active");
-    lelfContainer.classList.toggle("active");
-    rightContainer.classList.toggle("active");
-
-
-} )
 
 dismissButton.addEventListener("click",()=>{
     successContainer.classList.toggle("active");
@@ -20,14 +16,22 @@ dismissButton.addEventListener("click",()=>{
 
 } )
 
-const emailError = document.getElementById("email-error");
-const emailInput = document.getElementById("emailtext");
+
 
 function validateEmail() {
     if (!emailInput.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
         emailError.textContent = "Valid email required";
-        return false;
-    }
-
+        emailInput.classList.toggle("active");
+    }else{
     emailError.textContent = "";
+    successContainer.classList.toggle("active");
+    lelfContainer.classList.toggle("active");
+    rightContainer.classList.toggle("active");  
 }
+}
+ 
+subscribeButton.addEventListener("click", () => {
+    localStorage.setItem('emailInput', emailInput);
+    strongEmail.textContent = emailInput.value;
+    validateEmail();
+});
